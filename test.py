59f8dad0084467a123cabf04c2b56bf253fcf18b
Haneesh H <Haneesh.H@gds.ey.com>
2020-10-09 22:28:15 +0530

    initial commit

--- test.py
+++ test.py
@@ -66,16 +66,16 @@ def predict(table_data, queries):
   write_tf_example("results/sqa/tf_examples/test.tfrecord", examples)
   write_tf_example("results/sqa/tf_examples/random-split-1-dev.tfrecord", [])
   
-  ! python tapas/tapas/run_task_main.py \
-    --task="SQA" \
-    --output_dir="results" \
-    --noloop_predict \
-    --test_batch_size={len(queries)} \
-    --tapas_verbosity="ERROR" \
-    --compression_type= \
-    --init_checkpoint="tapas_sqa_base/model.ckpt" \
-    --bert_config_file="tapas_sqa_base/bert_config.json" \
-    --mode="predict" 2> error
+  # ! python tapas/tapas/run_task_main.py \
+  #   --task="SQA" \
+  #   --output_dir="results" \
+  #   --noloop_predict \
+  #   --test_batch_size={len(queries)} \
+  #   --tapas_verbosity="ERROR" \
+  #   --compression_type= \
+  #   --init_checkpoint="tapas_sqa_base/model.ckpt" \
+  #   --bert_config_file="tapas_sqa_base/bert_config.json" \
+  #   --mode="predict" 2> error
 
 
   results_path = "results/sqa/model/test_sequence.tsv"
